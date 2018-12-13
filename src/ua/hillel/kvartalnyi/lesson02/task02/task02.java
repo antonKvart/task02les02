b6f8b02b7923f9685c040b5079e741b2ab775bcc
Anton Kvartalnyi <anton.kvart@gmail.com>
2018-12-13 20:55:56 +0200

    update

--- src/ua/hillel/kvartalnyi/lesson02/task02/task02.java
+++ src/ua/hillel/kvartalnyi/lesson02/task02/task02.java
@@ -1,16 +1,17 @@
 package ua.hillel.kvartalnyi.lesson02.task02;
+
 import java.util.*;
+
 public class task02 {
     public static void main(String[] args) {
-        int n = 10;
-        n=n+1;
-        int mas[]= new int[n];
-        for (int i = 0; i < mas.length;i++) {
-            mas[i] = i;
+        Scanner n;
+        n = new Scanner(System.in);
+        System.out.println("Ввод размер массива");
+        int num = n.nextInt();
+        int mas[] = new int[num];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = i+1;
         }
-            System.out.print(Arrays.toString(mas));
-
+        System.out.print(Arrays.toString(mas));
     }
 }
-
-
